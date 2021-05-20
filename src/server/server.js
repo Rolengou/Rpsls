7d29cdaab52b2fc92e197145f17885a8f9dd8cb8
Rolengou <65602005+Rolengou@users.noreply.github.com>
2021-05-20 22:49:22 +0300

    wtf1

--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,15 @@
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const express = require("express");
-
+const http = require("http");
 const path = require("path");
 const useSocket = require("socket.io");
 const RpslsLogic = require('../GameLogic/rspls.js')
 
-const server = express();
+const index = path.join(__dirname, 'src')
 
-
-server.use(express.static(path.join(__dirname, "src")))
-server.listen(PORT,() => console.log("Server started on port 3000..."));
+const server = express()
+    .use((req, res) => {res.sendFile(index)})
+    .listen(PORT, () => console.log("listening on port 5000"))
 
 const io = useSocket(server)
 
@@ -31,7 +31,41 @@ io.on('connection', socket => {
     socket.on('message', messageText => {
         io.emit('message', messageText)
     })
-    })
+})
 
-
-
+// const app = express();
+// const server = http.createServer(app)
+//
+// app.use(express.static(path.join(__dirname, "src")))
+//
+// const io = useSocket(server, {
+//     cors: {
+//         origin: '*',
+//         methods: ["GET", "POST"],
+//         credentials: true,
+//         transports: ['websocket']
+//     }
+// })
+//
+// let waitingPlayer = null
+//
+// io.on('connection', socket => {
+//     socket.emit('connection')
+//
+//     if (waitingPlayer) {
+//         waitingPlayer.emit('redirect')
+//         RpslsLogic(waitingPlayer, socket)
+//         waitingPlayer = null
+//     } else {
+//         waitingPlayer = socket
+//         waitingPlayer.emit('message', 'Waiting for an opponent')
+//     }
+//
+//
+//     socket.on('message', messageText => {
+//         io.emit('message', messageText)
+//     })
+//     })
+//
+// server.listen(PORT,() => console.log("Server started on port 5000..."));
+//
