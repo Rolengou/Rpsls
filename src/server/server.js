8765beaaafd8d65fa2b24c77437ae0b5c1d95e47
Rolengou <65602005+Rolengou@users.noreply.github.com>
2021-05-20 22:16:56 +0300

    hmmmmmmmmmmm

--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,14 @@
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 3000;
 const express = require("express");
 const http = require("http");
 const path = require("path");
 const useSocket = require("socket.io");
 const RpslsLogic = require('../GameLogic/rspls.js')
 
-const app = express();
-const server = http.createServer(app)
+const server = express();
 
-app.use(express.static(path.join(__dirname, "src")))
+
+server.use(express.static(path.join(__dirname, "src")))
 
 const io = useSocket(server, {
     cors: {
@@ -40,7 +39,5 @@ io.on('connection', socket => {
     })
     })
 
-
-
-server.listen(PORT,() => console.log("Server started on port 5000..."));
+server.listen(PORT,() => console.log("Server started on port 3000..."));
 
