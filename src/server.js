984a651f30f53b80bc0cb2482a38f0aae37e4bf4
Rolengou <65602005+Rolengou@users.noreply.github.com>
2021-05-21 12:44:34 +0300

    11

--- src/server.js
+++ src/server.js
@@ -1,38 +1,22 @@
-// const express = require("express");
-// const http = require("http");
-// const path = require("path");
-// const useSocket = require("socket.io");
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const useSocket = require("socket.io");
 const RpslsLogic = require('./GameLogic/rspls.js')
 
-// const app = express();
-// const server = http.createServer(app)
-//
-// app.use(express.static(path.join(__dirname, "src")))
-//
-// const io = useSocket(server, {
-//     cors: {
-//         origin: '*',
-//         methods: ["GET", "POST"],
-//         credentials: true,
-//         transports: ['websocket']
-//     }
-// })
-//
-const express = require('express');
-const path = require('path');
-
 const app = express();
+const server = http.createServer(app)
 
-/***SERVER***/
-const port = process.env.PORT || 5000;
-server = app.listen(port, function(){
-    console.log('App is listening on port ' + port)
-});
+app.use(express.static(path.join(__dirname, "src")))
 
-
-/***SOCKET.IO***/
-const socket = require('socket.io');
-io = socket(server);
+const io = useSocket(server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"],
+        credentials: true,
+        transports: ['websocket']
+    }
+})
 
 let waitingPlayer = null
 
@@ -53,10 +37,7 @@ io.on('connection', socket => {
         io.emit('message', messageText)
     })
     })
-// // const port = process.env.PORT || 3000
-//
-// const ip='127.0.0.1';
-// const port ='3001' ;
-//
-// server.listen( ip, port, () => console.log("Server started on port 3000..."));
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => console.log("Server started on port 3000..."));
 
