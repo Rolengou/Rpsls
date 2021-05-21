792f84d5ff1d1fa69d7c6151e96eadeca07c237c
Rolengou <65602005+Rolengou@users.noreply.github.com>
2021-05-21 13:54:37 +0300

    12

--- src/server.js
+++ src/server.js
@@ -38,6 +38,6 @@ io.on('connection', socket => {
     })
     })
 
-const PORT = process.env.PORT || 3000
-server.listen(PORT, () => console.log("Server started on port 3000..."));
+const PORT = process.env.PORT || 5000
+server.listen(PORT, () => console.log("Server started on port 5000..."));
 
